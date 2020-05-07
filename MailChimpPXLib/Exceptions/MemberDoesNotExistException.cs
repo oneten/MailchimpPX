768d7d5d7b625604c974bfd128c8331e34996a9f
annie <annie@onetencommunications.com>
2020-05-07 15:20:05 -0400

    Making MemberDoesNotExistException public so I could use it in my Acuamtica code
    Also copy changes to the mailchimp project directly into the Acumatica
    Bin to make my Acumatica mailchimp dll's update right away when I make
    changes to the mailchimp project in visual studio

--- MailChimpPXLib/Exceptions/MemberDoesNotExistException.cs
+++ MailChimpPXLib/Exceptions/MemberDoesNotExistException.cs
@@ -4,7 +4,7 @@ using System.Runtime.Serialization;
 namespace MailchimpPXLib
 {
     [Serializable]
-    class MemberDoesNotExistException : Exception
+    public class MemberDoesNotExistException : Exception
     {
         public MemberDoesNotExistException() : base("Mailchimp member does not exist on list.")
         {
