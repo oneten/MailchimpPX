ee1b9f0882247f46279c73b16874ab66cbcfeb1e
annie <annie@onetencommunications.com>
2020-05-20 11:03:51 -0400

    Adding NoEmailException, working on exception handling (not done yet)

---
+++ MailChimpPXLib/Exceptions/NoEmailException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailChimpPXLib.Exceptions
+{
+    [Serializable]
+    public class NoEmailException : Exception
+    {
+        public NoEmailException() : base("The contact does not have an email address and therefore cannot be found in Mailchimp.")
+        {
+        }
+
+        public NoEmailException(string message) : base(message)
+        {
+        }
+    }
+}
