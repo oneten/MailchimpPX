fa68aa1918d32ab11f3d1ab76a08ab18de30247a
annie <annie@onetencommunications.com>
2025-04-04 11:45:01 -0400

    2024R2 Upgrade -- initial work (not done yet)

--- CustomizationSource/Mailchimp/App_RuntimeCode/MailchimpAttributes.cs
+++ CustomizationSource/Mailchimp/App_RuntimeCode/MailchimpAttributes.cs
@@ -1,4 +1,5 @@
 ﻿using PX.Data;
+using PX.Data.BQL;
 using PX.Objects.CR;
 using System;
 using System.Collections.Generic;
@@ -8,24 +9,27 @@ using System.Web;
 /// <summary>
 /// Summary description for Attributes
 /// </summary>
+/// 
 namespace Mailchimp
 {
     public class ActivityTypesApplicationAttribute : PXIntListAttribute
     {
         public ActivityTypesApplicationAttribute()
             : base(
-                new[] { Portal, Backend, Integration },
-                new[] { "Portal", "Backend", "Integration" })
+                new[] { Portal, Backend, System, Integration },
+                new[] { "Portal", "Backend", "System", "Integration" })
         {
         }
         
         public const int Portal = 0;
         public const int Backend = 1;
-        public const int Integration = 2;
+        //public const int Integration = 2; // Prior to 2024 R2, we used 2 for Integration. In 2024 R2, Acuamtica used 2 for System.
+        public const int System = 2;
+        public const int Integration = 3;
 
-        public class portal : Constant<int> { public portal() : base(Portal) { } }
-        public class backend : Constant<int> { public backend() : base(Backend) { } }
-        public class integration : Constant<int> { public integration() : base(Integration) { } }
+        public class portal : BqlType<IBqlInt, int>.Constant<portal> { public portal() : base(Portal) { } }
+        public class backend : BqlType<IBqlInt, int>.Constant<backend> { public backend() : base(Backend) { } }
+        public class system : BqlType<IBqlInt, int>.Constant<system> { public system() : base(System) { } }
+        public class integration : BqlType<IBqlInt, int>.Constant<integration> { public integration() : base(Integration) { } }
     }
-
 }
